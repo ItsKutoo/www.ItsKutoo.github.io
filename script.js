b091041a6b1a782bde12375185bb9947abdd8c55
Katie Ryan <katieryan1104@gmail.com>
2023-11-11 21:53:13 +0000

    Set themes for users when they leave page it stays the theme they chose before they left & default is light

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 console.log('Its working')
 
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+    setTheme('light')
+}else{
+    setTheme(theme)
+}
+
 let themeDots = document.getElementsByClassName('theme-dot')
 
 for (var i=0; themeDots.length > i; i++){
@@ -26,4 +34,6 @@ function setTheme(mode){
     if(mode == 'purple'){
         document.getElementById('theme-style').href = 'purple.css'
     }
+
+     localStorage.setItem('theme', mode)
 }
