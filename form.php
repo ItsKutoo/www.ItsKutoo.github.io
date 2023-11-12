e3757b12d148d0e48bee0e4da25df7f82de809f8
ItsKutoo <123093778+ItsKutoo@users.noreply.github.com>
2023-11-12 17:20:49 +0000

    added files

---
+++ form.php
@@ -0,0 +1,34 @@
+<?php
+
+
+if($_POST["message"]) {
+
+
+mail("katieryan1104@gmail.com", "Here is the subject line",
+
+
+$_POST["insert your message here"]. "From: an@email.address");
+
+
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1">
+    
+    <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@500&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="index.css">
+
+</head>
+<body>
+
+</body>
+</html>
